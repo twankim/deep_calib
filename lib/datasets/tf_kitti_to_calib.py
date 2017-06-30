810bfcf57a2e9427ddc85285a5e7cfe46a647635
twankim <twankim@utexas.edu>
2017-06-30 15:39:25 -0500

    Minor update (In development)

--- lib/datasets/tf_kitti_to_calib.py
+++ lib/datasets/tf_kitti_to_calib.py
@@ -2,7 +2,7 @@
 # @Author: twankim
 # @Date:   2017-06-26 16:55:00
 # @Last Modified by:   twankim
-# @Last Modified time: 2017-06-30 14:00:49
+# @Last Modified time: 2017-06-30 15:38:48
 
 from __future__ import absolute_import
 from __future__ import division
@@ -12,6 +12,7 @@ import argparse
 import sys
 import os
 import glob
+import cv2
 
 import numpy as np
 import tensorflow as tf
@@ -23,13 +24,19 @@ _TYPE_CALIB = 'calib'
 _TYPE_IMAGE = 'image_2'
 _TYPE_VELO = 'velodyne'
 
+_FORMAT_CALIB = '.txt'
+_FORMAT_IMAGE = '.png'
+_FORMAT_VELO = '.bin'
+
 _DIR_CALIB = 'data_{}_{}'.format(_TASK,_TYPE_CALIB)
 _DIR_IMAGE = 'data_{}_{}'.format(_TASK,_TYPE_IMAGE)
 _DIR_VELO = 'data_{}_{}'.format(_TASK,_TYPE_VELO)
 
+_NUM_GEN = 5 # Number of random generation per image
+
 _SET_CALIB = ['P2','R0_rect','Tr_velo_to_cam']
 
-def conv_calib(vid,cal_val):
+def get_calib_mat(vid,cal_val):
     assert vid in cal_set, 'Wrong parsing occurred: {}'.format(vid)
     if vid == cal_set[0]: # P2
         return cal_val.reshape((3,4))
@@ -43,6 +50,34 @@ def conv_calib(vid,cal_val):
         cal_mat[:3,:4] = cal_val.reshape((3,4))
         cal_mat[3,3] = 1
         return cal_mat
+
+def coord_transform(points, t_mat):
+    # Change to homogeneous form
+    points = np.hstack([points,np.ones((np.shape(points)[0],1))])
+    t_points = np.dot(points,t_mat.T)
+    # Normalize
+    t_points = t_points[:,:-1]/t_points[:,[-1]]
+    return t_points
+
+def project_velo_to_img(dict_calib,points,im_height,im_width):
+    # Extract depth data first before projection to 2d image space
+    trans_mat = np.dot(dict_calib[_SET_CALIB[1]],dict_calib[_SET_CALIB[2]])
+    points3D = coord_transform(points,trans_mat)
+    pointsDist = points3D[:,2]
+
+    # Project to image space
+    trans_mat = np.dot(dict_calib[_SET_CALIB[0]],trans_mat)
+    points2D = coord_transform(points,trans_mat)
+
+    # Find only feasible points
+    idx1 = (points2D[:,0]>=0) & (points2D[:,0] <=im_width-1)
+    idx2 = (points2D[:,1]>=0) & (points2D[:,1] <=im_height-1)
+    idx3 = (pointsDist>=0)
+    idx_in = idx1 & idx2 & idx3
+    points2D_fin = points2D[idx_in,:]
+    pointsDist_fin = pointsDist[idx_in]
+
+    return points2D_fin, pointsDist_fin
 
 # Product of quaternions
 def qprod(q_a,q_b):
@@ -98,7 +133,7 @@ def gen_decalib(max_theta, max_dist):
     param_decalib = {}
 
     # Rotation angle
-    param_decalib['rot'] = np.random.uniform(-max_theata,max_theta)
+    param_decalib['rot'] = np.random.uniform(-max_theta,max_theta)
 
     # Rotation axis (unit vector)
     a_vec = np.random.standard_normal(3)
@@ -119,9 +154,14 @@ def gen_decalib(max_theta, max_dist):
     q_t[1:] = param_decalib['trans']
     param_decalib['q_t'] = q_t
 
+    param_decalib['y'] = np.concatenate((q_r,q_t[1:]))
+
     return param_decalib
 
 def main(args):
+    max_theta = args.max_theta
+    max_dist = args.max_dist
+
     path_kitti = args.path_kitti
     assert os.path.exists(path_kitti),\
             "Download KITTI Dataset or enter correct path"
@@ -131,15 +171,18 @@ def main(args):
         os.makedirs(path_out)
 
     path_cal = os.path.join(path_kitti,_TASK,_DIR_CALIB)
-    path_velo = os.path.join(path_kitti,_TASK)
+    path_velo = os.path.join(path_kitti,_TASK,_DIR_VELO)
+    path_img = os.path.join(path_kitti,_TASK,_DIR_IMAGE)
 
     for image_set in ['training','testing']:
-        imList = glob.glob(os.path.join(path_cal,image_set,_TYPE_CALIB,'*.txt'))
+        imList = glob.glob(os.path.join(path_cal,image_set,_TYPE_CALIB,'*'+_FORMAT_CALIB))
         imList.sort()
         imNames = [os.path.split(d)[1].strip('.txt') for d in imList]
 
+        num_data = len(imNames)*_NUM_GEN # Number of data to be gerated
+
         for iter, imName in enumearate(imNames):
-            f_calib = os.path.join(path_cal,image_set,_TYPE_CALIB,imName+'.txt')
+            f_calib = os.path.join(path_cal,image_set,_TYPE_CALIB,imName+_FORMAT_CALIB)
             # Read & Save calibration matrix
             with open(f_calib,'r'):
                 temp_dict = {}
@@ -148,10 +191,37 @@ def main(args):
                         vid,vals = line.split(':',1)
                         val_cal = [float(v) for v in vals.split()]
                         if vid in _SET_CALIB:
-                            temp_dict[vid] = conv_calib(vid,np.array(val_cal))
+                            temp_dict[vid] = get_calib_mat(vid,np.array(val_cal))
 
             # Read velodyne points
-            f_velo = os.path.join()
+            f_velo = os.path.join(path_velo,image_set,_TYPE_VELO,imName+_FORMAT_VELO)
+            points_org = np.fromfile(f_velo,dtype=np.float32).reshape(-1,4)
+            points = points_org[:,:3] # exclude points reflectance
+
+            # Read image file
+            f_img = os.path.join(path_img,image_set,_TYPE_IMAGE,imName+_FORMAT_IMAGE)
+            im = cv2.imread(f_img)
+            im = im[:,:,(2,1,0)] # BGR to RGB
+            im_height,im_width = np.shape(im)[0:2]
+
+            # Project velodyne points to image plane
+            points2D, pointsDist = project_velo_to_img(temp_dict,
+                                                       points,
+                                                       im_height,
+                                                       im_width)
+
+            # ------- Generate random ratation for decalibration data --------
+            # Generate random rotation
+            for i_ran in xrange(_NUM_GEN):
+                param_decalib = gen_decalib(max_theta, max_dist)
+                ran_dict = temp_dict.copy()
+                ran_dict[_SET_CALIB[2]] = dualquat_to_transmat(param_decalib['q_r'],
+                                                               param_decalib['q_t'])
+            
+                points2D_ran, pointsDist_ran = project_velo_to_img(ran_dict,
+                                                                   points,
+                                                                   im_height,
+                                                                   im_width)
 
 
 def parse_args():
@@ -166,6 +236,12 @@ def parse_args():
     parser.add_argument('-dir_out', dest='path_out',
                         help='Path to save tfrecord kitti dataset',
                         default = '/data/tf/kitti_calib', type = str)
+    parser.add_argument('-max_theta', dest=max_theta,
+                        help='Range of rotation angle in degree [-theta,+theta)',
+                        default = 10, type=float)
+    parser.add_argument('-max_dist', dest=max_dist,
+                        help='Maximum translation distance in meter',
+                        default = 1.5, type=float)
     args = parser.parse_args()
     return args
 
