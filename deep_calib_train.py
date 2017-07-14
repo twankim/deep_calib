03e74d15172f8320dc651129a6151a605184b8f9
twankim <twankim@utexas.edu>
2017-07-14 16:49:19 -0500

    Under development

--- deep_calib_train.py
+++ deep_calib_train.py
@@ -446,7 +446,7 @@ def main(_):
               num_threads=FLAGS.num_preprocessing_threads,
               capacity=5 * FLAGS.batch_size)
       batch_queue = slim.prefetch_queue.prefetch_queue(
-              [images,lidars,y_trues], capacity=2 * deploy_config.num_clones)
+              [images,lidars,y_trues], capacity=3 * deploy_config.num_clones)
 
     ####################
     # Define the model #
