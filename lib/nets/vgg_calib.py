6248b9264f47c487abc56b6689cb0f9ecb704e12
twankim <twankim@utexas.edu>
2017-07-20 10:57:05 -0500

    Added scripts

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -203,37 +203,42 @@ def vgg_16(images,
       # net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
       # net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
-      # ConvNets for lidar
-      net2 = slim.repeat(lidars, 2, slim.conv2d, 32, [3, 3], scope='conv1_lidar')
-      net2 = slim.max_pool2d(net2, [2, 2], scope='pool1_lidar')
-      net2 = slim.repeat(net2, 2, slim.conv2d, 64, [3, 3], scope='conv2_lidar')
-      net2 = slim.max_pool2d(net2, [2, 2], scope='pool2_lidar')
-      net2 = slim.repeat(net2, 3, slim.conv2d, 128, [3, 3], scope='conv3_lidar')
-      net2 = slim.max_pool2d(net2, [2, 2], scope='pool3_lidar')
-      # net2 = slim.repeat(net2, 2, slim.conv2d, 512, [3, 3], scope='conv4_lidar')
-      # net2 = slim.max_pool2d(net2, [2, 2], scope='pool4_lidar')
-      # net2 = slim.repeat(net2, 2, slim.conv2d, 512, [3, 3], scope='conv5_lidar')
-      # net2 = slim.max_pool2d(net2, [2, 2], scope='pool5_lidar')
+      with tf.variable_scope('lidar_feat'):
+        # ConvNets for lidar
+        net2 = slim.repeat(lidars, 2, slim.conv2d, 32, [3, 3], scope='conv1_lidar')
+        net2 = slim.max_pool2d(net2, [2, 2], scope='pool1_lidar')
+        net2 = slim.repeat(net2, 2, slim.conv2d, 64, [3, 3], scope='conv2_lidar')
+        net2 = slim.max_pool2d(net2, [2, 2], scope='pool2_lidar')
+        net2 = slim.repeat(net2, 3, slim.conv2d, 128, [3, 3], scope='conv3_lidar')
+        net2 = slim.max_pool2d(net2, [2, 2], scope='pool3_lidar')
+        # net2 = slim.repeat(net2, 2, slim.conv2d, 512, [3, 3], scope='conv4_lidar')
+        # net2 = slim.max_pool2d(net2, [2, 2], scope='pool4_lidar')
+        # net2 = slim.repeat(net2, 2, slim.conv2d, 512, [3, 3], scope='conv5_lidar')
+        # net2 = slim.max_pool2d(net2, [2, 2], scope='pool5_lidar')
 
       # Concat two channels
       net = tf.concat(values=[net,net2],axis=3)
 
-      # Remaining ConvNets for Feature Matching
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
-      net = slim.max_pool2d(net, [2, 2], scope='pool4')
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
-      net = slim.max_pool2d(net, [2, 2], scope='pool5')
-      # Use conv2d instead of fully_connected layers.
-      net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
-      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                         scope='dropout6')
-      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                         scope='dropout7')
-      net = slim.conv2d(net, num_preds, [1, 1],
-                        activation_fn=None,
-                        normalizer_fn=None,
-                        scope='fc8')
+      with tf.variable_scope('match_feat'):
+        # Remaining ConvNets for Feature Matching
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+        net = slim.max_pool2d(net, [2, 2], scope='pool4')
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+        net = slim.max_pool2d(net, [2, 2], scope='pool5')
+
+      with tf.variable_scope('regression'):
+        # Use conv2d instead of fully_connected layers.
+        net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
+        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                           scope='dropout6')
+        net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                           scope='dropout7')
+        net = slim.conv2d(net, num_preds, [1, 1],
+                          activation_fn=None,
+                          normalizer_fn=None,
+                          scope='fc8')
+      
       # Convert end_points_collection into a end_point dict.
       end_points = slim.utils.convert_collection_to_dict(end_points_collection)
       if spatial_squeeze:
---
+++ scripts/train_vgg_16_gpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+
+MODEL_NAME=vgg_16
+
+CUDA_VISIBLE_DEVICES=1 \
+python deep_calib_train.py \
+    --dataset_dir=data_ex/tf/kitti_calib \
+    --train_dir=checkpoints/kitti_calib \
+    --list_param=20,1.5 \
+    --clone_on_cpu=False \
+    --model_name=${MODEL_NAME} \
+    --checkpoint_path=pretrained/${MODEL_NAME}.ckpt \
+    --checkpoint_exclude_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression
+    # --trainable_scopes=
+    # --ignore_missing_vars=
