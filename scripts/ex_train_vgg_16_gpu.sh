2b78838b099e1f9004d016bf0915ff65ee56b1b9
twankim <twankim@utexas.edu>
2017-07-20 10:59:03 -0500

    Added scripts

--- scripts/ex_train_vgg_16_gpu.sh
+++ scripts/ex_train_vgg_16_gpu.sh
@@ -5,7 +5,7 @@ MODEL_NAME=vgg_16
 
 CUDA_VISIBLE_DEVICES=1 \
 python deep_calib_train.py \
-    --dataset_dir=/data/tf/kitti_calib \
+    --dataset_dir=data_ex/tf/kitti_calib \
     --train_dir=checkpoints/kitti_calib \
     --list_param=20,1.5 \
     --clone_on_cpu=False \
