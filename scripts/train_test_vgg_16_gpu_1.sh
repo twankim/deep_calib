9f4023d261b28d847090a040e95abd6d637408d2
twankim <twankim@utexas.edu>
2017-08-14 15:13:12 -0500

    Script update for small data 5,0.5

--- scripts/train_test_vgg_16_gpu_1.sh
+++ scripts/train_test_vgg_16_gpu_1.sh
@@ -5,24 +5,26 @@ MODEL_NAME=vgg_16
 WEIGHT_LOSS=1
 LEARNING_RATE=0.0001
 END_LEARNING_RATE=0.000001
+DATA_NAME=kitti_calib_small
+LIST_PARAM=5,0.5
 
 python deep_calib_train.py \
-    --dataset_dir=/data/tf/kitti_calib \
-    --train_dir=/data/tf/checkpoints/kitti_calib \
+    --dataset_dir=/data/tf/${DATA_NAME} \
+    --train_dir=/data/tf/checkpoints/${DATA_NAME} \
     --max_number_of_steps=50000 \
-    --list_param=20,1.5 \
+    --list_param=${LIST_PARAM} \
     --clone_on_cpu=False \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=pretrained/${MODEL_NAME}.ckpt \
     --checkpoint_exclude_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression \
     --learning_rate=${LEARNING_RATE} \
     --end_learning_rate=${END_LEARNING_RATE}
-    # --trainable_scopes=
+    # --trainable_scopes=vgg_16/lidar_feat,vgg_16/match_feat,vgg_16/regression
     # --ignore_missing_vars=
 
 python deep_calib_test.py \
-    --dataset_dir=/data/tf/kitti_calib \
-    --checkpoint_path=/data/tf/checkpoints/kitti_calib/${MODEL_NAME}/weight_${WEIGHT_LOSS} \
-    --list_param=20,1.5 \
+    --dataset_dir=/data/tf/${DATA_NAME} \
+    --checkpoint_path=/data/tf/checkpoints/${DATA_NAME}/${MODEL_NAME}/weight_${WEIGHT_LOSS} \
+    --list_param=${LIST_PARAM} \
     --model_name=${MODEL_NAME} \
     --weight_loss=${WEIGHT_LOSS}
--- scripts/train_test_vgg_16_gpu_10.sh
+++ scripts/train_test_vgg_16_gpu_10.sh
@@ -6,12 +6,13 @@ WEIGHT_LOSS=10
 LEARNING_RATE=0.0001
 END_LEARNING_RATE=0.000001
 DATA_NAME=kitti_calib_small
+LIST_PARAM=5,0.5
 
 python deep_calib_train.py \
     --dataset_dir=/data/tf/${DATA_NAME} \
     --train_dir=/data/tf/checkpoints/${DATA_NAME} \
     --max_number_of_steps=50000 \
-    --list_param=20,1.5 \
+    --list_param=${LIST_PARAM} \
     --weight_loss=${WEIGHT_LOSS} \
     --clone_on_cpu=False \
     --model_name=${MODEL_NAME} \
@@ -25,6 +26,6 @@ python deep_calib_train.py \
 python deep_calib_test.py \
     --dataset_dir=/data/tf/${DATA_NAME} \
     --checkpoint_path=/data/tf/checkpoints/${DATA_NAME}/${MODEL_NAME}/weight_${WEIGHT_LOSS} \
-    --list_param=20,1.5 \
+    --list_param=${LIST_PARAM} \
     --model_name=${MODEL_NAME} \
     --weight_loss=${WEIGHT_LOSS}
