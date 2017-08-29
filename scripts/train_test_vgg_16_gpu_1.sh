f871c23029b6d265a13e837341c67240b1fceeec
twankim <twankim@utexas.edu>
2017-08-29 16:35:43 -0500

    Changed Reg Arch. & scripts

--- scripts/train_test_vgg_16_gpu_1.sh
+++ scripts/train_test_vgg_16_gpu_1.sh
@@ -3,23 +3,27 @@
 
 MODEL_NAME=vgg_16
 WEIGHT_LOSS=1
-LEARNING_RATE=0.0001
-END_LEARNING_RATE=0.000001
+BATCH_SIZE=16
+LEARNING_RATE=0.00001
+END_LEARNING_RATE=0.0000001
 DATA_NAME=kitti_calib_small
 LIST_PARAM=5,0.5
+SUMMARY_SECS=600
 
 python deep_calib_train.py \
+    --save_summaries_secs=${SUMMARY_SECS} \
     --dataset_dir=/data/tf/${DATA_NAME} \
     --train_dir=/data/tf/checkpoints/${DATA_NAME} \
-    --max_number_of_steps=10000 \
+    --max_number_of_steps=80000 \
+    --batch_size=${BATCH_SIZE} \
     --list_param=${LIST_PARAM} \
     --clone_on_cpu=False \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=pretrained/${MODEL_NAME}.ckpt \
     --checkpoint_exclude_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression \
     --learning_rate=${LEARNING_RATE} \
-    --end_learning_rate=${END_LEARNING_RATE}
-    # --trainable_scopes=vgg_16/lidar_feat,vgg_16/match_feat,vgg_16/regression
+    --end_learning_rate=${END_LEARNING_RATE} \
+    --trainable_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression
     # --ignore_missing_vars=
 
 python deep_calib_test.py \
