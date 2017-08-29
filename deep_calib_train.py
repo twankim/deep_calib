83aa4f6bd4b8694f6ff733ee35e5f8cfcc12a8d6
twankim <twankim@utexas.edu>
2017-08-29 15:01:39 -0500

    delete normalization of quaternion

--- deep_calib_train.py
+++ deep_calib_train.py
@@ -191,7 +191,7 @@ tf.app.flags.DEFINE_string(
     'as `None`, then the model_name flag is used.')
 
 tf.app.flags.DEFINE_integer(
-    'batch_size', 32, 'The number of samples in each batch.')
+    'batch_size', 16, 'The number of samples in each batch.')
 
 tf.app.flags.DEFINE_integer(
     'train_image_size', None, 'Train image size')
