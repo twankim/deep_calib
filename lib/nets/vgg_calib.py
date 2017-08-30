d87a6f191d2a35561730eede9699a94f146a9735
twankim <twankim@utexas.edu>
2017-08-30 01:26:57 -0500

    Debugging..

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -280,12 +280,12 @@ def vgg_16(images,
 
       with tf.variable_scope('regression'):
         # Use conv2d instead of fully_connected layers.
-        # net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
-        net = slim.conv2d(net, 1024, [7, 7], padding=fc_conv_padding, scope='fc6')
+        net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
+        # net = slim.conv2d(net, 1024, [7, 7], padding=fc_conv_padding, scope='fc6')
         net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                            scope='dropout6')
-        # net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-        net = slim.conv2d(net, 1024, [1, 1], scope='fc7')
+        net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+        # net = slim.conv2d(net, 1024, [1, 1], scope='fc7')
         net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                            scope='dropout7')
         # net = last_layer(net,num_preds)
