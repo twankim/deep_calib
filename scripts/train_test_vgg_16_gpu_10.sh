13b0b6bb4fe2c5746b93185c9b7a87fa29cbb927
twankim <twankim@utexas.edu>
2017-09-03 23:12:23 -0500

    Testing Learning Rate

--- scripts/train_test_vgg_16_gpu_10.sh
+++ scripts/train_test_vgg_16_gpu_10.sh
@@ -4,8 +4,8 @@
 MODEL_NAME=vgg_16
 WEIGHT_LOSS=10
 BATCH_SIZE=32
-LEARNING_RATE=0.00000001
-END_LEARNING_RATE=0.0000000001
+LEARNING_RATE=0.0000001
+END_LEARNING_RATE=0.000000001
 DATA_NAME=kitti_calib_small
 LIST_PARAM=5,0.5
 LIDAR_POOL=5,2
@@ -24,7 +24,7 @@ python deep_calib_train.py \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=pretrained/${MODEL_NAME}.ckpt \
     --checkpoint_exclude_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression \
-    --trainable_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression \
+    # --trainable_scopes=${MODEL_NAME}/lidar_feat,${MODEL_NAME}/match_feat,${MODEL_NAME}/regression \
     --learning_rate=${LEARNING_RATE} \
     --end_learning_rate=${END_LEARNING_RATE}
     # --ignore_missing_vars=
