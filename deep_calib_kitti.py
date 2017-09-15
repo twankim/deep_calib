3e42c691e43a35405f9bd731672b6bcd7c8a5ea1
twankim <twankim@utexas.edu>
2017-09-15 11:11:37 -0500

    Fixed minor error in yr_to_qr

--- deep_calib_kitti.py
+++ deep_calib_kitti.py
@@ -347,7 +347,7 @@ def main(_):
       # Calibarte based on the prediction
       cal_dict = ran_dict.copy()
 
-      Rt = quat_to_transmat(q_r_preds,y_r_preds[4:])
+      Rt = quat_to_transmat(q_r_preds,y_preds_val[4:])
       Rt_cal = Rt.copy()
       Rt_cal[:3,:3] = Rt[:3,:3].T
       Rt_cal[:3,3] = -np.dot(Rt[:3,:3].T,Rt[:3,3])
