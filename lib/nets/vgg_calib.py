19a93a4285f39f5d1dc5f072a7df3439e530f54e
twankim <twankim@utexas.edu>
2017-09-22 17:18:34 -0500

    Testing VGG Network

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -110,7 +110,6 @@ def vgg_arg_scope(weight_decay=0.0005):
   """
   with slim.arg_scope([slim.conv2d],
                       activation_fn=tf.nn.relu,
-                      weights_initializer=tf.contrib.layers.xavier_initializer(),
                       weights_regularizer=slim.l2_regularizer(weight_decay),
                       biases_initializer=tf.constant_initializer(0.01)):
                       # biases_initializer=tf.zeros_initializer()):
@@ -158,8 +157,8 @@ def vgg_16(images,
       net = slim.max_pool2d(net, [2, 2], scope='pool1')
       net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
       net = slim.max_pool2d(net, [2, 2], scope='pool2')
-      # net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
-      # net = slim.max_pool2d(net, [2, 2], scope='pool3')
+      net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+      net = slim.max_pool2d(net, [2, 2], scope='pool3')
 
       with tf.variable_scope('lidar_feat'):
         # ConvNets for lidar
@@ -167,8 +166,8 @@ def vgg_16(images,
         net2 = slim.max_pool2d(net2, [2, 2], scope='pool1_lidar')
         net2 = slim.repeat(net2, 2, slim.conv2d, 64, [3, 3], scope='conv2_lidar')
         net2 = slim.max_pool2d(net2, [2, 2], scope='pool2_lidar')
-        # net2 = slim.repeat(net2, 2, slim.conv2d, 128, [3, 3], scope='conv3_lidar')
-        # net2 = slim.max_pool2d(net2, [2, 2], scope='pool3_lidar')
+        net2 = slim.repeat(net2, 2, slim.conv2d, 128, [3, 3], scope='conv3_lidar')
+        net2 = slim.max_pool2d(net2, [2, 2], scope='pool3_lidar')
       
       # Concat two channels
       net = tf.concat(values=[net,net2],axis=3)
@@ -177,16 +176,16 @@ def vgg_16(images,
       with slim.arg_scope([slim.conv2d],normalizer_fn=slim.batch_norm):
         with tf.variable_scope('match_feat'):
           # Remaining ConvNets for Feature Matching
-          net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv4')
+          net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv4')
           net = slim.max_pool2d(net, [2, 2], scope='pool4')
-          # net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv5')
-          # net = slim.max_pool2d(net, [2, 2], scope='pool5')
+          net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv5')
+          net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
         with tf.variable_scope('regression'):
           # Use conv2d instead of fully_connected layers.
-          net = slim.conv2d(net, 128, [28, 28], padding=fc_conv_padding, scope='fc6')
-          # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-          #                    scope='dropout6')
+          net = slim.conv2d(net, 256, [7, 7], padding=fc_conv_padding, scope='fc6')
+          net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                             scope='dropout6')
           # net = slim.conv2d(net, 256, [1, 1], scope='fc7')
           # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
           #                    scope='dropout7')
