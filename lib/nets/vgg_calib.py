bdfbd395d9c2333d80d296bb1f5accba09f280b7
twankim <twankim@utexas.edu>
2017-09-22 15:34:25 -0500

    Testing VGG Network

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -158,7 +158,7 @@ def vgg_16(images,
       net = slim.max_pool2d(net, [2, 2], scope='pool1')
       net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
       net = slim.max_pool2d(net, [2, 2], scope='pool2')
-      net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv3')
+      net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
       net = slim.max_pool2d(net, [2, 2], scope='pool3')
 
       with tf.variable_scope('lidar_feat'):
@@ -179,7 +179,7 @@ def vgg_16(images,
           # Remaining ConvNets for Feature Matching
           net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv4')
           net = slim.max_pool2d(net, [2, 2], scope='pool4')
-          net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv5')
+          net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv5')
           net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
         with tf.variable_scope('regression'):
