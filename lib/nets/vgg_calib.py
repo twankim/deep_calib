4f1620312faaee95c33a79659670c50d8443b3b1
twankim <twankim@utexas.edu>
2017-10-02 11:35:49 -0500

    Testing.. VGG_19

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -200,8 +200,8 @@ def vgg_19(images,
                         outputs_collections=end_points_collection):
       net = slim.repeat(images, 2, slim.conv2d, 64, [3, 3], scope='conv1')
       net = slim.max_pool2d(net, [2, 2], scope='pool1')
-      # net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
-      # net = slim.max_pool2d(net, [2, 2], scope='pool2')
+      net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+      net = slim.max_pool2d(net, [2, 2], scope='pool2')
       # net = slim.repeat(net, 4, slim.conv2d, 256, [3, 3], scope='conv3')
       # net = slim.max_pool2d(net, [2, 2], scope='pool3')
       
@@ -209,8 +209,8 @@ def vgg_19(images,
         # ConvNets for lidar
         net2 = slim.repeat(lidars, 2, slim.conv2d, 32, [3, 3], scope='conv1_lidar')
         net2 = slim.max_pool2d(net2, [2, 2], scope='pool1_lidar')
-        # net2 = slim.repeat(net2, 2, slim.conv2d, 64, [3, 3], scope='conv2_lidar')
-        # net2 = slim.max_pool2d(net2, [2, 2], scope='pool2_lidar')
+        net2 = slim.repeat(net2, 2, slim.conv2d, 64, [3, 3], scope='conv2_lidar')
+        net2 = slim.max_pool2d(net2, [2, 2], scope='pool2_lidar')
         # net2 = slim.repeat(net2, 4, slim.conv2d, 128, [3, 3], scope='conv3_lidar')
         # net2 = slim.max_pool2d(net2, [2, 2], scope='pool3_lidar')
       
@@ -228,7 +228,7 @@ def vgg_19(images,
 
       with tf.variable_scope('regression'):
         # Use conv2d instead of fully_connected layers.
-        net = slim.conv2d(net, 1024, [112, 112],
+        net = slim.conv2d(net, 4096, [56, 56],
                       padding=fc_conv_padding,
                       scope='fc6')
         # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
