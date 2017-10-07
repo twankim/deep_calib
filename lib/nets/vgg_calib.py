034ab44a93bbc92c7de77b540e25ea16e489e4ee
twankim <twankim@utexas.edu>
2017-10-06 23:46:21 -0500

    Updating KITTI eval function

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -218,17 +218,17 @@ def vgg_19(images,
       net = tf.concat(values=[net,net2],axis=3)
 
       # Apply Batch_Norm on rest of the network    
-      # with slim.arg_scope([slim.conv2d],normalizer_fn=slim.batch_norm):
-      #   with tf.variable_scope('match_feat'):
-      #     # Remaining ConvNets for Feature Matching
-      #     net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv4')
-      #     net = slim.max_pool2d(net, [2, 2], scope='pool4')
+      with slim.arg_scope([slim.conv2d],normalizer_fn=slim.batch_norm):
+        with tf.variable_scope('match_feat'):
+          # Remaining ConvNets for Feature Matching
+          net = slim.repeat(net, 2, slim.conv2d, 256, [3, 3], scope='conv4')
+          net = slim.max_pool2d(net, [2, 2], scope='pool4')
       #     net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv5')
       #     net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
       with tf.variable_scope('regression'):
         # Use conv2d instead of fully_connected layers.
-        net = slim.conv2d(net, 4096, [56, 56],
+        net = slim.conv2d(net, 4096, [27, 27],
                       padding=fc_conv_padding,
                       scope='fc6')
         # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
