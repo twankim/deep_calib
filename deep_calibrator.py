ea6c3f94aa07c0f4ea467f70f3a2b2c08c788ef5
twankim <twankim@utexas.edu>
2017-10-06 22:23:52 -0500

    Testing batch size

--- deep_calibrator.py
+++ deep_calibrator.py
@@ -46,12 +46,13 @@ class Predictor:
 
         with slim.arg_scope(factory_nets.arg_scopes_map[self.model_name]()):
             # Crop image and lidar to consider only sensed region
-            image,lidar = tf_prepare_test(im_placeholder,im_depth_placeholder,
-                                          params_crop)
+            image,lidar = tf_prepare_test(self.im_placeholder,
+                                          self.im_depth_placeholder,
+                                          self.params_crop)
             image,lidar = self.preprocessing_fn(image,lidar,
-                                                test_image_size,
-                                                test_image_size,
-                                                pool_size=lidar_pool)
+                                                self.test_image_size,
+                                                self.test_image_size,
+                                                pool_size=self.lidar_pool)
 
             # Change format to [batch_size, height, width, channels]
             images = tf.expand_dims(image, 0)
