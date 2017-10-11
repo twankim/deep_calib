9a8699b165e1f7a0691d4062b75d4df3d6c8ec30
twankim <twankim@utexas.edu>
2017-10-11 17:43:36 -0500

    Testing...

--- lib/nets/vgg_calib.py
+++ lib/nets/vgg_calib.py
@@ -137,14 +137,15 @@ def vgg_16(images,
           net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv5')
           net = slim.repeat(net, 2, slim.conv2d, 512, [1, 1], scope='conv5_1x1')
           net = slim.max_pool2d(net, [2, 2], scope='pool5')
-          # net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv6')
-          # net = slim.max_pool2d(net, [2, 2], scope='pool6')
+          net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv6')
+          net = slim.repeat(net, 2, slim.conv2d, 512, [1, 1], scope='conv6_1x1')
+          net = slim.max_pool2d(net, [2, 2], scope='pool6')
 
       with tf.variable_scope('regression'):
         # Use conv2d instead of fully_connected layers.
-        net = slim.conv2d(net, 1024, [7, 7],
-                      padding=fc_conv_padding,
-                      scope='fc6')
+        # net = slim.conv2d(net, 1024, [7, 7],
+        #               padding=fc_conv_padding,
+        #               scope='fc6')
         # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
         #                    scope='dropout6')
         # net = slim.conv2d(net, 256, [1, 1],
