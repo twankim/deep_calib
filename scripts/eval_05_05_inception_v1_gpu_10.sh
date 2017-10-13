05152e01c4f13f42210a068306188978222594cb
twankim <twankim@utexas.edu>
2017-10-13 00:32:28 -0500

    Testing Inception

---
+++ scripts/eval_05_05_inception_v1_gpu_10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+MODEL_NAME=inception_v1
+WEIGHT_LOSS=10
+LOG_NAME=kitti_calib_05_05
+LIST_PARAM=5,0.5
+LIDAR_POOL=4,2
+
+python deep_calib_kitti.py \
+    --checkpoint_path=/data/tf/checkpoints/${LOG_NAME}/${MODEL_NAME}/weight_${WEIGHT_LOSS} \
+    --list_param=${LIST_PARAM} \
+    --lidar_pool=${LIDAR_POOL} \
+    --model_name=${MODEL_NAME} \
+    --weight_loss=${WEIGHT_LOSS} \
+    --dir_image=data_ex/kitti/object/data_object_image_2/testing/image_2 \
+    --dir_lidar=data_ex/kitti/object/data_object_velodyne/testing/velodyne \
+    --dir_calib=data_ex/kitti/object/data_object_calib/testing/calib \
+    --dir_out=data_ex/results/${MODEL_NAME}/weight_${WEIGHT_LOSS}
